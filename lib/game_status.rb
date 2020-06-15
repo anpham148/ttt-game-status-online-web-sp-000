4edf92f12433ded049749c1d653b7f07a9167fa1
An Pham <anpham148@gmail.com>
2020-06-15 12:45:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,21 +7,19 @@ end
 WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-  # position_taken?(board, index) = false
-  # WIN_COMBINATIONS.each do |win_combination|
-  # position_taken?(board, index) = win_combination.all? do |board_index|
-  #     board[board_index] == "X" || board[board_index] == "O"
-  #   end
-  # end
-  # position_taken?(board,index)
+  position_taken?(board, index) = false
+  WIN_COMBINATIONS.each do |win_combination|
+  position_taken?(board, index) = win_combination.all? do |board_index|
+      board[board_index] == "X" || board[board_index] == "O"
+    end
+  end
+  position_taken?(board,index)
 end
 
 def full?(board)
-
   board.all? {|index| board[index] == "X" || board[index] == "O" }
-
 end
 
-def draw?()
+def draw?(boards)
 
 end
