466bac560894f46df1737142340391e4d043605b
An Pham <anpham148@gmail.com>
2020-06-15 12:23:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,9 +7,11 @@ end
 WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
+  win = false
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination.all? do |character|
-      character == character
+    win_combination.each do |board_index|
+      board[board_index] == "X" || board[board_index] == "O"
     end
   end
+  win
 end
